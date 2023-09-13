9eafc4410b1bdde7e3f29387416f51fc5fc3105a
davidzdjelar <144233382+davidzdjelar@users.noreply.github.com>
2023-09-13 09:56:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var memberBtn = document.getElementById("memberBtn");
+    var skillsBtn = document.getElementById("skillsBtn");
+
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberBtn.style.backgroundColor = "#f2f2f2";
+    skillsBtn.style.backgroundColor = "#ffffff";
+}
